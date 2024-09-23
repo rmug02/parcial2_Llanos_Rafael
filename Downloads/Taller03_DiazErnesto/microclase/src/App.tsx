b84870e6fe10555c22d17bfd388aba4f71c41305
Ernesto <ediaz41@cuc.edu.co>
2024-09-23 09:36:26 -0500

    m1

---
+++ Downloads/Taller03_DiazErnesto/microclase/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import myLogo from './assets//Tesla_Motors.svg.png'
+
+function App() { 
+  return (
+    <>
+      <div>
+        <img src={myLogo} alt="React logo" />
+        <h1> Tesla </h1>
+        <p> The brand that revolutionizes the automotive industry.... </p>
+      </div>
+    </>
+  )
+}
+
+export default App
